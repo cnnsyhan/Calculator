77c83d51b292e0c0a8367b8e7e07a8d6b9137b1d
Can Seyhan <muhamedseyhan503@gmail.com>
2020-10-28 21:57:39 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Can Seyhan on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
